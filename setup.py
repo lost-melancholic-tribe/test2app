a627f55b531d1e6176b53394ffe4cf28bce104fb
romer8 <gio.busrom@gmail.com>
2023-03-02 10:59:40 -0700

    0.0.15

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ resource_files = find_all_resource_files(app_package, TethysAppBase.package_name
 
 setup(
     name=release_package,
-    version='0.0.11',
+    version='0.0.15',
     description='',
     long_description='',
     keywords='',
